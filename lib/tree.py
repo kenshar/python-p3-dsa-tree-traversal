258c49a8459758c84f15d90c222f78eda5b773c5
kenshar <kenkar252@gmail.com>
2025-12-02 10:50:28 +0300

    init

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,39 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if not self.root:
+      return None
+
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    return None
+
+
+def breadth_first_traversal(node):
+  result = []
+  nodes_to_visit = [node]
+
+  while len(nodes_to_visit) > 0:
+    node = nodes_to_visit.pop(0)
+    result.append(node['value'])
+    nodes_to_visit = nodes_to_visit + node['children']
+
+  return result
+
+
+def depth_first_traversal(node):
+  result = []
+  nodes_to_visit = [node]
+
+  while len(nodes_to_visit) > 0:
+    node = nodes_to_visit.pop(0)
+    result.append(node['value'])
+    nodes_to_visit = node['children'] + nodes_to_visit
+
+  return result
